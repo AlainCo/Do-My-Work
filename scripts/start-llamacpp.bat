15c2a1cc16a45b8ad7033550c394d4daaf862fd5
AlainCo <13969182+AlainCo@users.noreply.github.com>
2026-06-01 00:41:50 +0200

    llamacpp env variable for nb core

--- scripts/start-llamacpp.bat
+++ scripts/start-llamacpp.bat
@@ -7,16 +7,21 @@ if "%LLAMA_HOME%"=="" (
 )
 echo Using llama server from LLAMA_HOME: %LLAMA_HOME%
 
+if "%LLAMA_NBCORE%"=="" (
+    echo LLAMA_NBCORE environment variable is not set. Please set it to the nuble of real processor cores on your llama server installation.
+    exit /b 1
+)
+
 set PORT=8000
 set CTXSIZE=8192
 set MODEL=Ministral-3-3B-Instruct-2512-Q4_K_M.gguf
 
-set APPOPTS=-m "%LLAMA_HOME%\model\%MODEL%" --ctx-size %CTXSIZE% 
+set APPOPTS=--model "%LLAMA_HOME%\model\%MODEL%" --ctx-size %CTXSIZE% 
 set TRACEOPTS=
 rem set TRACEOPTS=%TRACEOPTS% --verbose 
 set TRACEOPTS=%TRACEOPTS% --metrics
 set NETOPTS=--host 127.0.0.1 --port %PORT%
-set PERFOPTS=-t 12  --batch-size 512 --ubatch-size 512  --mlock
+set PERFOPTS=--threads %LLAMA_NBCORE%  --batch-size 512 --ubatch-size 512  --mlock
 
 @echo on
 "%LLAMA_HOME%\llama-server.exe" %APPOPTS% %NETOPTS% %PERFOPTS% %TRACEOPTS%

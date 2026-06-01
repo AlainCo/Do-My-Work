d6e834aabd8a26c4e40317c6619f72c6d3cde5ca
AlainCo <13969182+AlainCo@users.noreply.github.com>
2026-06-01 07:40:25 +0200

    llamacpp launche bash

--- scripts/start-llamacpp.sh
+++ scripts/start-llamacpp.sh
@@ -2,11 +2,20 @@
 set -euo pipefail
 
 LLAMA_HOME_DIR="${LLAMA_HOME:-}"
+LLAMA_NBCORE_N="${LLAMA_NBCORE:-}"
+
 
 if [ -n "${LLAMA_HOME_DIR}" ] && [ -d "${LLAMA_HOME_DIR}" ]; then
   echo "Using llama server from LLAMA_HOME: ${LLAMA_HOME_DIR}"
 else
   echo "LLAMA_HOME is not set or does not point to a valid directory. Please set LLAMA_HOME to the path of your llama server installation."
+  exit 1
+fi
+
+if [ -n "${LLAMA_NBCORE_N}" ] && [ "${LLAMA_NBCORE_N}" -ge 1 ]; then
+  echo "Using llama server from LLAMA_NBCORE: ${LLAMA_NBCORE}"
+else
+  echo "LLAMA_NBCORE is not set or is not positive. Please set LLAMA_NBCORE to the number of cores to use ."
   exit 1
 fi
 
@@ -29,7 +38,7 @@ NET_OPTS=(
 )
 
 PERF_OPTS=(
-  -t 12
+  -t "${LLAMA_NBCORE_N}"
   --batch-size 512
   --ubatch-size 512
   --mlock

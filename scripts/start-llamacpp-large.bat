ac14219f116969d70313b23f726719e126a1bda1
AlainCo <13969182+AlainCo@users.noreply.github.com>
2026-06-02 13:14:24 +0200

    create bat scripts for various model, create config for Mistral mini

---
+++ scripts/start-llamacpp-large.bat
@@ -0,0 +1,10 @@
+@setlocal
+@echo off
+set SCRIPTDIR=%~dp0.
+:: set LLAMA_MODEL=Ministral-3-3B-Instruct-2512-Q4_K_M.gguf
+:: set LLAMA_MODEL=Ministral-3-8B-Instruct-2512-Q5_K_M.gguf
+set LLAMA_MODEL=Ministral-3-14B-Instruct-2512-Q4_K_M.gguf
+:: set LLAMA_MODEL=Mistral-Small-3.2-24B-Instruct-2506-Q4_K_M.gguf
+set LLAMA_CONTEXT=4096
+%SCRIPTDIR%\start-llamacpp.bat
+@endlocal
--- scripts/start-llamacpp.bat
+++ scripts/start-llamacpp.bat
@@ -5,24 +5,50 @@ if "%LLAMA_HOME%"=="" (
     echo LLAMA_HOME environment variable is not set. Please set it to the path of your llama server installation.
     exit /b 1
 )
-echo Using llama server from LLAMA_HOME: %LLAMA_HOME%
+echo Using llamacpp server from LLAMA_HOME: %LLAMA_HOME%
 
 if "%LLAMA_NBCORE%"=="" (
-    echo LLAMA_NBCORE environment variable is not set. Please set it to the nuble of real processor cores on your llama server installation.
+    echo LLAMA_NBCORE environment variable is not set. Please set it to the number of real processor cores on your llama server installation.
     exit /b 1
 )
 
-set PORT=8000
-set CTXSIZE=4096
-rem set MODEL=Ministral-3-3B-Instruct-2512-Q4_K_M.gguf
-set MODEL=Ministral-3-8B-Instruct-2512-Q5_K_M.gguf
-rem set MODEL=Ministral-3-14B-Instruct-2512-Q4_K_M.gguf
+echo Using llamacpp core number: %LLAMA_NBCORE%
 
-set APPOPTS=--model "%LLAMA_HOME%\model\%MODEL%" --ctx-size %CTXSIZE% 
+if "%LLAMA_MODEL%"=="" (
+    echo LLAMA_MODEL environment variable is not set. Please set it to the name of the llama model you want to use.
+    exit /b 1
+)
+
+echo Using llamacpp model: %LLAMA_MODEL%
+
+if "%LLAMA_PORT%"=="" (
+    echo LLAMA_PORT environment variable is not set. Set to 8000.
+    set LLAMA_PORT=8000
+)
+
+echo Using llamacpp port: %LLAMA_PORT%
+
+if "%LLAMA_CONTEXT%"=="" (
+    echo LLAMA_CONTEXT environment variable is not set. Set to 4096.
+    set LLAMA_CONTEXT=4096
+)
+
+echo Using llamacpp context size: %LLAMA_CONTEXT%
+
+
+
+
+set PORT=8000
+set LLAMA_CONTEXT=4096
+rem set LLAMA_MODEL=Ministral-3-3B-Instruct-2512-Q4_K_M.gguf
+rem set LLAMA_MODEL=Ministral-3-8B-Instruct-2512-Q5_K_M.gguf
+rem set LLAMA_MODEL=Ministral-3-14B-Instruct-2512-Q4_K_M.gguf
+
+set APPOPTS=--model "%LLAMA_HOME%\model\%LLAMA_MODEL%" --ctx-size %LLAMA_CONTEXT% 
 set TRACEOPTS=
 rem set TRACEOPTS=%TRACEOPTS% --verbose --metrics 
-set TRACEOPTS=%TRACEOPTS% --log-verbosity 4
-set NETOPTS=--host 127.0.0.1 --port %PORT%
+set TRACEOPTS=%TRACEOPTS% --log-verbosity 3
+set NETOPTS=--host 127.0.0.1 --port %LLAMA_PORT%
 set PERFOPTS=--threads %LLAMA_NBCORE%  --batch-size 512 --ubatch-size 512  --mlock
 
 @echo on

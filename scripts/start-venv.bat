d8d310f3371b73d7bdcc4e337a7a085dbb8da4da
AlainCo <13969182+AlainCo@users.noreply.github.com>
2026-06-05 09:21:54 +0200

    script to help dev

--- scripts/start-venv.bat
+++ scripts/start-venv.bat
@@ -3,6 +3,7 @@
 cd /d %~dp0..
 call .venv\Scripts\activate.bat
 echo Python virtual env activated in %CD%
+:: app path to scripts
 set PATH=%CD%\scripts;%PATH%
 echo Path added toward: %CD%\scripts
 cmd /k

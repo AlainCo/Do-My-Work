1ce91cdca04b0d9a570cd377a73166a8314c402e
AlainCo <13969182+AlainCo@users.noreply.github.com>
2026-06-06 16:59:36 +0200

    modele medium, refonte prompt

--- scripts/start-llamacpp-medium.bat
+++ scripts/start-llamacpp-medium.bat
@@ -1,6 +1,6 @@
 @setlocal
 @echo off
 set SCRIPTDIR=%~dp0.
-set LLAMA_ARG_MODEL=Ministral-3-8B-Instruct-2512-Q5_K_M.gguf
+set LLAMA_ARG_MODEL=Ministral-3-8B-Instruct-2512-Q4_K_M.gguf
 %SCRIPTDIR%\start-llamacpp.bat
 @endlocal
